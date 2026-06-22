460a12b75cc0a386aa03325b8faeb52561520933
ChahatModi18 <chahatmodi17@gmail.com>
2026-06-22 11:14:34 +0530

    Time: 0 ms (100%), Space: 30.8 MB (33.15%) - LeetHub

---
+++ 3674-minimum-operations-to-equalize-array/3674-minimum-operations-to-equalize-array.cpp
@@ -0,0 +1,14 @@
+class Solution { //sliding window variable size
+
+public:
+    int minOperations(vector<int>& nums) {
+        int n=nums[0];
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=n){
+                return 1;
+            }
+
+        }
+        return 0;
+    }
+};
