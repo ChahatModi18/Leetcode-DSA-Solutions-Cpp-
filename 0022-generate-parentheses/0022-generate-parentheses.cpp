b9ce5ecdab89c07c808b123632763a336abbe51d
ChahatModi18 <chahatmodi17@gmail.com>
2026-06-23 11:35:46 +0530

    Time: 3 ms (70.19%), Space: 13.5 MB (73.5%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -7,6 +7,7 @@ public:
     // Recursive backtracking function
     void back(string& curr, int open, int close, int n) {
 
+
         // Base Case:
         // When the string length becomes 2*n,
         // we have used all parentheses.
@@ -14,6 +15,7 @@ public:
             result.push_back(curr);   // Store the valid combination
             return;
         }
+
 
         // Choice 1: Add an opening bracket '('
         // We can add it only if we have not used n opening brackets yet.
@@ -23,6 +25,7 @@ public:
             curr.pop_back();             // Undo choice (Backtrack)
             //using pop to remove last char in string using STL
         }
+
 
         // Choice 2: Add a closing bracket ')'
         // We can add it only when there are more opening
