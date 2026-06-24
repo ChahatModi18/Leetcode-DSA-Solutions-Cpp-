349d5c64350b5f59cefa47f120bcea83d4394cdc
ChahatModi18 <chahatmodi17@gmail.com>
2026-06-24 10:17:40 +0530

    Time: 11 ms (66.37%), Space: 35 MB (53.32%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,28 +1,60 @@
 class Solution {
 public:
+    // Function to calculate how many days are needed, if ship capacity is "capacity"
+     int returnDays(vector<int>& weights, int capacity) {
+
+        int days = 1;      // start with Day 1
+        int load = 0;      // current ship load
+
+        for(int weight : weights) {
+
+            // If adding current package exceeds capacity,
+            // send current ship and start a new day
+            if(load + weight > capacity) {
+                days++;
+                load = weight;
+            }
+            else {
+                load += weight;
+            }
+        }
+
+        return days;
+    }
+
     int shipWithinDays(vector<int>& weights, int days) {
-        int maxWeight = -1, totalWeight = 0;
-        for (int weight : weights) {
-            maxWeight = max(maxWeight, weight);
-            totalWeight = totalWeight + weight;
+
+    //sorted array in increasing order 
+    //binary search pattern: find optimal answer
+    //optimization algorithm 
+    //ship size depends on no of days not on weight of ship 
+
+            // Minimum capacity must be at least the heaviest package
+        int mx = *max_element(weights.begin(), weights.end()); //max array= starting point 
+
+        // Maximum capacity can be sum of all packages
+        int sum = accumulate(weights.begin(), weights.end(), 0); //sum array= ending point 
+
+        int low = mx;
+        int high = sum;
+
+
+        // Binary Search on Answer
+        while(low <= high){
+            int mid = low+(high-low)/2;
+
+            int requiredDays = returnDays(weights, mid); //standard template for binary search  : // a function needs to be defined in some qns in binary search
+
+
+        // Capacity works, try smaller capacity
+        if (requiredDays <= days){
+             high=mid-1;
         }
-        //here weight and total weight work as left and right pointer of bunary search
-        while (maxWeight < totalWeight) {
-            int midWeight = maxWeight + (totalWeight - maxWeight) / 2;
-            int daysNeeded = 1, currWeight = 0;
-            for (int weight : weights) {
-                if (currWeight + weight > midWeight) {
-                    daysNeeded++;
-                    currWeight = 0;
-                }
-                currWeight = currWeight + weight;
-            }
-            if (daysNeeded > days) {
-                maxWeight = midWeight + 1;
-            } else {
-                totalWeight = midWeight;
-            }
+        else{
+            low=mid+1; // if no of adays is more , going right side 
         }
-        return maxWeight;
+        }
+        //Smallest valid capacity
+        return low; 
     }
 };
