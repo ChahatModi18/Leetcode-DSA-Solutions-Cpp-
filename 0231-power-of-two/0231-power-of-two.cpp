9be383495946ffe9962917ab27956095aca657a4
ChahatModi18 <chahatmodi17@gmail.com>
2026-06-27 09:41:18 +0530

    Time: 0 ms (100%), Space: 7.9 MB (49.16%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,8 +1,7 @@
-class Solution {  // Reminder-divident method
+class Solution {//ceil floor method
 public:
     bool isPowerOfTwo(int n) {
-        if(n==0) return false;
-        while(n%2==0) n/=2;
-        return n==1;
+        if(n <= 0) return false;
+        return ceil(log2(n)) == floor(log2(n));
     }
 };
