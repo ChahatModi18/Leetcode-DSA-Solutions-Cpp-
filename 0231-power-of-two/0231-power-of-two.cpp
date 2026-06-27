948d5ff539eb98543308018dae59715bc1cd91de
ChahatModi18 <chahatmodi17@gmail.com>
2026-06-27 09:40:22 +0530

    Time: 3 ms (3.42%), Space: 7.9 MB (11.85%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,7 +1,8 @@
-class Solution {  //Reminder-divident method via Recursion
+class Solution {  // Reminder-divident method
 public:
     bool isPowerOfTwo(int n) {
         if(n==0) return false;
-        return ((n==1) || (n%2==0 && isPowerOfTwo(n/2)));
+        while(n%2==0) n/=2;
+        return n==1;
     }
 };
