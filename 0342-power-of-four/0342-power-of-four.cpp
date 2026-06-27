cae3969bccd91d30365632f77a46cfe9542005d5
ChahatModi18 <chahatmodi17@gmail.com>
2026-06-27 09:43:41 +0530

    Time: 0 ms (100%), Space: 8 MB (10.83%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if (n<=0) return 0;
-        int exp=31-countl_zero((unsigned)n);// log2(n)
-        return (exp&1)==0 && n==1<<exp;
+        if (n<0) return 0;
+        bitset<31> x(n);
+        if (x.count()!=1) return 0;
+        for(int i=0; i<31; i+=2)
+            if(x[i]) return 1;
+        return 0;
     }
 };
