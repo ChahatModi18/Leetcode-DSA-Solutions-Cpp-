d4853df58b2c4e0263ce44efed698cc86e5d4687
ChahatModi18 <chahatmodi17@gmail.com>
2026-06-27 09:41:46 +0530

    Time: 0 ms (100%), Space: 7.9 MB (49.16%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,7 +1,12 @@
-class Solution {//ceil floor method
+class Solution { //C++ STL method
 public:
     bool isPowerOfTwo(int n) {
-        if(n <= 0) return false;
-        return ceil(log2(n)) == floor(log2(n));
+        if(n<0)return false;
+        
+        int bits=__builtin_popcount(n);
+        
+        if(bits==1)
+            return true;
+        return false;
     }
 };
