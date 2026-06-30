35428c93b190d760bb75d20e773555d83935eac5
ChahatModi18 <chahatmodi17@gmail.com>
2026-06-30 12:04:20 +0530

    Time: 0 ms (100%), Space: 101.2 MB (5.07%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,19 +1,17 @@
-class Solution {
+class Solution { //dp solution 
 public:
-    int maxProfit(vector<int>& prices) {
-        int maxCur = 0;    // Maximum profit ending at current day
-        int maxSoFar = 0;  // Maximum profit found so far
 
-        for (int i = 1; i < prices.size(); i++) {
+    int maxProfit(vector<int>& a) {
+        int ans=0;
+        int n=a.size();
+        vector<int>pM(n);
+        pM[0]=a[0];
 
-            // Add today's price difference to current profit.
-            // If it becomes negative, reset to 0 (start a new subarray).
-            maxCur = max(0, maxCur + (prices[i] - prices[i - 1]));
-
-            // Update the maximum profit obtained so far.
-            maxSoFar = max(maxSoFar, maxCur);
+        for(int i=1;i<n;i++){
+            pM[i]=min(a[i],pM[i-1]);
+            ans=max(ans,a[i]-pM[i]);
         }
 
-        return maxSoFar;
+        return ans;
     }
 };
