044d52e990196102a19821bbb8e653ca66de1670
ChahatModi18 <chahatmodi17@gmail.com>
2026-07-01 09:28:52 +0530

    Time: 0 ms (100%), Space: 8.6 MB (80.22%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,15 @@
+class Solution { // ITERATION 
+public:
+    int addDigits(int num) {
+        int sum=0;
+        while(num>=10){
+            while(num>0){
+            sum+=(num%10);
+            num/=10;
+        }
+        num=sum;
+        sum=0;
+        }
+        return num;
+    }
+};
