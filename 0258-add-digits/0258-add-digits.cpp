8e042fc101734336d06d8ae3b90c7affc375d8ff
ChahatModi18 <chahatmodi17@gmail.com>
2026-07-01 09:33:26 +0530

    Time: 0 ms (100%), Space: 8.7 MB (46.55%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -2,12 +2,12 @@ class Solution { // ITERATION
 public:
     int addDigits(int num) {
         int sum=0;
-        while(num>=10){
-            while(num>0){
-            sum+=(num%10);
-            num/=10;
+        while(num>=10){ //The outer while loop checks whether the number has more than one digit (num >= 10).
+            while(num>0){ //The inner while loop extracts every digit and adds it to sum
+            sum+=(num%10);  //extracts last digit
+            num/=10; //removes last digit 
         }
-        num=sum;
+        num=sum; //After one complete pass, num = sum  : sum = 0 /:Repeat until num is a single digit.
         sum=0;
         }
         return num;
