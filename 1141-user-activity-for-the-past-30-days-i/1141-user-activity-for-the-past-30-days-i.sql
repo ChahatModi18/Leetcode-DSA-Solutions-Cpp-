d03bf194950d4efba412c1e135f904d8bd129bec
ChahatModi18 <chahatmodi17@gmail.com>
2026-08-13 09:47:37 +0530

    Time: 634 ms (15.66%), Space: 0B (100%) - LeetHub

--- 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -8,6 +8,20 @@ and activity_date<='2019-07-27'
 group by activity_date;
 
 
+# where date_sub('2019-07-27',interval 30 day) < activity_date 
+# MEANS
+# activity_date must be greater than 30 days before July 27.
+
+/*
+SELECT activity_date AS day,
+       COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE activity_date > '2019-06-27'
+  AND activity_date <= '2019-07-27'
+GROUP BY activity_date;
+*/
+
+
 /*
 SELECT
     activity_date AS day,
