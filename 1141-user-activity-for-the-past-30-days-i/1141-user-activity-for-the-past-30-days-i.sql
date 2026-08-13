9ec274b1cf6006a9ad9031db451151e22af8647a
ChahatModi18 <chahatmodi17@gmail.com>
2026-08-13 09:39:07 +0530

    Time: 539 ms (43.8%), Space: 0B (100%) - LeetHub

---
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -0,0 +1,27 @@
+# Write your MySQL query statement below
+#ind the number of unique active users for each day during the 30-day period ending on 2019-07-27.
+select activity_date as day,
+count(distinct user_id) as active_users 
+from activity
+where date_sub('2019-07-27',interval 30 day) < activity_date
+and activity_date<='2019-07-27'
+group by activity_date;
+
+
+/*
+SELECT
+    activity_date AS day,
+    COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE activity_date BETWEEN DATE '2019-06-28' AND DATE '2019-07-27'
+GROUP BY activity_date;
+*/
+
+/*
+SELECT
+    activity_date AS day,
+    COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-27'
+GROUP BY activity_date;
+*/
