05b67d26db84798ac8fcf486221c713061f67477
ChahatModi18 <chahatmodi17@gmail.com>
2026-08-13 09:00:03 +0530

    Time: 581 ms (37.59%), Space: 0B (100%) - LeetHub

--- 1075-project-employees-i/1075-project-employees-i.sql
+++ 1075-project-employees-i/1075-project-employees-i.sql
@@ -4,3 +4,22 @@ FROM PROJECT P
 JOIN EMPLOYEE E
 ON P.EMPLOYEE_ID=E.EMPLOYEE_ID
 GROUP BY P.PROJECT_ID;
+
+
+/*# Write your MySQL query statement below
+SELECT p.project_id, ROUND(AVG(e.experience_years),2) AS average_years
+FROM Project p 
+LEFT JOIN Employee e
+ON p.employee_id = e.employee_id
+GROUP BY p.project_id
+
+
+
+
+LEFT JOIN means:
+Keep every row from Project, even if there is no matching Employee.
+
+
+INNER JOIN says:
+Keep only projects whose employee has a matching row in Employee.
+*/
