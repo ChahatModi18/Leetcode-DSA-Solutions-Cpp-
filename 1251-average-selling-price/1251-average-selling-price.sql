a69916ce743f30d704d6168d11a58cebc66fd54c
ChahatModi18 <chahatmodi17@gmail.com>
2026-08-14 09:08:40 +0530

    Time: 849 ms (61.98%), Space: 0B (100%) - LeetHub

--- 1251-average-selling-price/1251-average-selling-price.sql
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -75,3 +75,24 @@ ON p.product_id = u.product_id
 AND u.purchase_date BETWEEN p.start_date AND p.end_date
 GROUP BY p.product_id;
 */
+
+
+/*
+most efficient code : 
+# Write your MySQL query statement below
+select p.product_id , COALESCE(ROUND(sum(p.price * u.units)/sum(u.units),2),0) average_price
+from prices p
+left join unitssold u
+on p.product_id = u.product_id
+where (u.purchase_date >= p.start_date and 
+u.purchase_date <= p.end_date)
+OR u.purchase_date is null
+group by p.product_id;
+
+-- select * from prices p 
+-- left join 
+-- unitssold u
+-- on p.product_id = u.product_id
+-- where u.purchase_date >= p.start_date and 
+-- u.purchase_date < p.end_date;
+*/
