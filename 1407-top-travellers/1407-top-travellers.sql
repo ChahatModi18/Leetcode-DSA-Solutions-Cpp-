cb119331a2c6e147d9f2d94ae7e36e166f07890b
ChahatModi18 <chahatmodi17@gmail.com>
2026-08-14 11:17:37 +0530

    Time: 910 ms (54.2%), Space: 0B (100%) - LeetHub

--- 1407-top-travellers/1407-top-travellers.sql
+++ 1407-top-travellers/1407-top-travellers.sql
@@ -9,3 +9,11 @@ FROM Users u
 LEFT JOIN Rides r
     ON u.id = r.user_id
 ORDER BY travelled_distance DESC, u.name ASC;
+
+
+/*SELECT name, COALESCE(SUM(distance), 0) AS travelled_distance
+FROM Users
+LEFT JOIN Rides ON Users.id = Rides.user_id
+GROUP BY Rides.user_id
+ORDER BY travelled_distance DESC, name ASC
+*/
