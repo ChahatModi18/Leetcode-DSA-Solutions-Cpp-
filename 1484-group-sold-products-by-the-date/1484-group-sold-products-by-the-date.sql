f212f62bbe09ec009b75d13c1930921a43030f1d
ChahatModi18 <chahatmodi17@gmail.com>
2026-08-14 11:38:45 +0530

    Time: 458 ms (73.81%), Space: 0B (100%) - LeetHub

--- 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
+++ 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
@@ -4,3 +4,29 @@ GROUP_CONCAT( DISTINCT product order by product ASC separator ',' ) as products
 from activities
 group by sell_date
 order by sell_date ;
+
+/*
+Without GROUP_CONCAT:
+Headphone
+Keyboard
+Mouse
+
+With:
+GROUP_CONCAT(product)
+you get:
+Headphone,Keyboard,Mouse
+
+So:
+GROUP_CONCAT = combine multiple rows into one value.
+
+Without DISTINCT:
+GROUP_CONCAT(product)
+→ Keyboard,Mouse,Keyboard
+
+With:
+GROUP_CONCAT(DISTINCT product)
+→ Keyboard,Mouse
+
+So:
+DISTINCT removes duplicate products
+*/
