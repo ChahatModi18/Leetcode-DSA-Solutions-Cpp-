08c39df01f88e2fef5faadc27ba3501d81b6d7bd
ChahatModi18 <chahatmodi17@gmail.com>
2026-08-14 09:26:27 +0530

    Time: 1142 ms (40.42%), Space: 0B (100%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -1,13 +1,35 @@
 # Write your MySQL query statement below
 select st.student_id,st.student_name,s.subject_name, count(e.student_id) as attended_exams
 from students st
-
 cross join subjects s
 left join examinations e
-
 on st.student_id=e.student_id
 and s.subject_name=e.subject_name
-
 group by st.student_id,st.student_name,s.subject_name
-
 order by st.student_id,st.student_name, s.subject_name ;
+
+
+#GROUP BY does NOT mean sort.
+#ORDER BY does NOT mean group.
+
+/*
+1. Why GROUP BY has 3 columns?
+Because we want one result for every student + subject combination.
+Each combination becomes one group.
+Then counts exams inside each group.
+
+ORDER BY
+
+How should the final rows be arranged?
+It first sorts by student_id.
+Within the same student, sort by name ;Within the same student, sort subjects alphabetically
+*/
+
+#Our goal is to list the subjects for each student.
+#cross join MEANS cartesian Product
+#means :hich is like making every possible pair of rows from two tables.
+/*Time to LEFT JOIN so we can show a '0' for subjects students didn't take, it will be clear later.
+Each attempt at Alice's exam is displayed one after another.
+And we want to count them.
+*/
+
