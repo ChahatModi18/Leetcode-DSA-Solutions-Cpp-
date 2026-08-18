d17b63e8d871f2dad6fb4834947fc43f0e011fe4
ChahatModi18 <chahatmodi17@gmail.com>
2026-08-18 11:11:53 +0530

    Time: 313 ms (36.52%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -21,6 +21,39 @@ SELECT (
 The inner query produces one value:
 200
 The outer query gives that value a column name:
-
 AS SecondHighestSalary
+
+
+
+
+
+SELECT (
+    SELECT DISTINCT salary
+    FROM (
+        SELECT 
+            salary,
+            DENSE_RANK() OVER (ORDER BY salary DESC) as rnk
+        FROM Employee
+    ) AS RankedSalaries
+    WHERE rnk = 2
+) AS SecondHighestSalary;
+
+
+DENSE_RANK() OVER (ORDER BY salary DESC)
+orts salaries from highest to lowest and assigns a rank.
+Because DENSE_RANK() gives the same rank to equal values.
+
+RankedSalaries
+----------------
+salary | rnk
+300    | 1
+200    | 2
+200    | 2
+100    | 3
+
+WHERE rnk = 2 means:
+Give me only the rows whose salary has rank 2.
+
+he outer query simply says:
+Display that value with the name SecondHighestSalary.
 */
