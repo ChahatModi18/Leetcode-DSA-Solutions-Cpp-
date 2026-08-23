888627d18b0543e96dd5961b4102922887b7f701
Chahat Modi <chahatmodi17@gmail.com>
2026-08-23 14:20:55 +0530

    Time: 898 ms (19.2%), Space: 0B (100%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -5,3 +5,30 @@ from sales where (product_id, year) in (
     from sales
     group by product_id
 )
+
+/*
+# Write your MySQL query statement below
+
+WITH CTE AS (
+    SELECT product_id, MIN(year) AS minyear 
+    FROM Sales 
+    GROUP BY product_id 
+)
+SELECT s.product_id, s.year AS first_year, s.quantity, s.price 
+FROM Sales s
+INNER JOIN CTE
+ ON cte.product_id = s.product_id  
+ AND 
+ s.year = cte.minyear; 
+
+explanation
+CTE as a temporary result table:
+
+explannation of 1st approach :
+
+WHERE (product_id, year) IN (...)
+This is a pair/row comparison.
+It essentially says:
+Keep the row if its (product_id, year) pair exists in the result of the subquery.
+
+*/
